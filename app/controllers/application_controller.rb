6be26cb580953860a5794a247a66fdc4e75613bb
Rian I. de Oliveira <rian_hp@outlook.com>
2014-12-28 15:08:36 -0200

    Tornar a página de login a página inicial da aplicação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #def after_sign_in_path_for(resource_or_scope)
-  #	'/inicio'
-  #end
+  def after_sign_in_path_for(resource_or_scope)
+  	start_page_path
+  end
 
-  #def after_sign_out_path_for(resource_or_scope)
-  #  '/users/sign_in'
-  #end
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  root to: 'visitors#index'
+  #root to: 'visitors#index'
 
   devise_for :users
 
-  #devise_scope :user do
- 	#	get "sign_in", to: "devise/sessions#new"
-	#end
+  devise_scope :user do
+  	root to: 'users/sessions#new'
+  	match 'start_page' => 'visitors#index', via: 'get'
+ 		#get "sign_in", to: "devise/sessions#new"
+	end
 end
