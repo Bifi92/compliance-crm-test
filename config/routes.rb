ff57d8f4c26b23cb1e3e64b75d238dfb7f31ce34
Rian I. de Oliveira <rian_hp@outlook.com>
2014-12-28 14:39:01 -0200

    Geração do controller e dos views para devise no escopo de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   root to: 'visitors#index'
+
   devise_for :users
+
+  #devise_scope :user do
+ 	#	get "sign_in", to: "devise/sessions#new"
+	#end
 end
