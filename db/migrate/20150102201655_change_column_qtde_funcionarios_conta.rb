2b7ef72e46106951b911b9053d2f63ea284415f7
Rian I. de Oliveira <rian_hp@outlook.com>
2015-01-05 17:44:26 -0200

    Sincronização com o heroku

--- db/migrate/20150102201655_change_column_qtde_funcionarios_conta.rb
+++ db/migrate/20150102201655_change_column_qtde_funcionarios_conta.rb
@@ -1,11 +1,11 @@
 class ChangeColumnQtdeFuncionariosConta < ActiveRecord::Migration
   def self.up
-    change_table :tablename do |t|
+    change_table :conta do |t|
       t.change :conta, :qtde_funcionarios, :string
     end
   end
   def self.down
-    change_table :tablename do |t|
+    change_table :conta do |t|
       t.change :conta, :qtde_funcionarios, :integer
     end
   end
