96aca523ce8698fc6381c928185dd67c8079eb27
Rian I. de Oliveira <rian_hp@outlook.com>
2015-01-21 18:29:37 -0200

    Correção do acesso direto por url a determinado objeto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource_or_scope)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource_or_scope) 
   	start_page_path
   end
 
@@ -18,8 +20,14 @@ class ApplicationController < ActionController::Base
 		end
 	end
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
+  def render_404
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
+      format.xml  { head :not_found }
+      format.any  { head :not_found }
+    end
+  end
+  
   protected
 
   def configure_permitted_parameters
--- app/controllers/contatos_controller.rb
+++ app/controllers/contatos_controller.rb
@@ -1,6 +1,7 @@
 class ContatosController < ApplicationController
   before_action :require_authentication
   before_action :set_contato, only: [:show, :edit, :update, :destroy]
+  before_action :check_user_owner, only: [:show, :edit]
 
   respond_to :html
 
@@ -43,10 +44,20 @@ class ContatosController < ApplicationController
 
   private
     def set_contato
-      @contato = Contato.find(params[:id])
+      begin
+        @contato = Contato.find(params[:id])
+      rescue
+        render_404
+      end
     end
 
     def contato_params
       params.require(:contato).permit(:contum_id, :contato_id, :saudacao, :sexo, :titulo, :nome, :sobrenome, :data_nascimento, :empresa, :departamento, :cpf, :descricao, :email_pessoal, :email_corporativo, :nome_assistente, :telefone_assistente, :data_devolucao_email, :motivo_devolucao_email, :recusar_emails, :fax, :recusar_fax, :telefone_residencial, :telefone_comercial, :celular, :outro_telefone, :recusar_telefonemas, :bairro_corre, :rua_corre, :numero_corre, :complemento_corre, :cep_corre, :bairro_corpo, :logradouro, :rua_corpo, :numero_corpo, :complemento_corpo, :cep_corpo, :esporte_preferido, :hobby, :excluido, :origem_lead, :countrycorre_country_id, :estadocorre_estado_id, :cidadecorre_cidade_id, :countrycorpo_country_id, :estadocorpo_estado_id, :cidadecorpo_cidade_id)
+    end
+
+    def check_user_owner
+      if @contato.proprietario_user_id != current_user.id
+        redirect_to contatos_path, alert: "You are not the owner of this object"
+      end
     end
 end
